93194e72cda771f85e49128aadfc2cd37d339f01
SandraChege <chegemwihaki88@gmail.com>
2022-12-07 12:31:22 -0800

    advance task 1

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    m = 0
+    n = 0
+
+    for tup in my_list:
+        m += p[0] * p[1]
+        n += p[1]
+
+    return (m / n)
